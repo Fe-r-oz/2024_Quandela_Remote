c2d00c9240f15a29a7ff773406645cf00315f7de
Feroz <93876775+Fe-r-oz@users.noreply.github.com>
2024-02-04 04:30:42 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-#This is an example of the file you must have in your main git branch
 import perceval as pcvl
 
-def get_CCZ() -> pcvl.Processor:
-    return pcvl.catalog["postprocessed ccz"].build_processor()
+def get_CCZ():
+    processor = pcvl.Processor("SLOS", 6)
+    processor.add(4, pcvl.BS.H())
+    processor.add(0, pcvl.catalog["heralded cz"].build_processor())
+    processor.add(4, pcvl.BS.H())
+
+    states = {
+        pcvl.BasicState([1, 0, 1, 0, 1, 0]): "000",
+        pcvl.BasicState([1, 0, 0, 1, 0, 1]): "001",
+        pcvl.BasicState([1, 0, 0, 1, 1, 0]): "010",
+        pcvl.BasicState([0, 1, 1, 0, 1, 0]): "100",
+        pcvl.BasicState([0, 1, 1, 0, 0, 1]): "101",
+        pcvl.BasicState([0, 1, 0, 1, 1, 0]): "110",
+        pcvl.BasicState([0, 1, 0, 1, 0, 1]): "111"
+    }
+
+    ca = pcvl.algorithm.Analyzer(processor, states)
+
+    truth_table = {"000": "000", "001": "001", "010": "010", "011": "011", "100": "100", "101": "101", "110": "110", "111": "111"}
+    ca.compute(expected=truth_table)
+
+    pcvl.pdisplay(ca)
+    print(f"performance = {ca.performance}, fidelity = {ca.fidelity.real}")
+
+# Uncomment the following line if you want to execute the code when main.py is run
+# get_CCZ()
